27e32363837d53387924867617fbb12c11ea352c
Nithin Ravi <nithinravi2bcommb@gmail.com>
2025-06-25 22:49:21 +0530

    fixed a small bug

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api', router);
 
+app.get('/', (req, res) => {
+  res.send('Server is live');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
